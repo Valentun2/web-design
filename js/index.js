206462e8b3380eb4c887ffde1386c64e70e27bcd
Valentun2 <pocomaxawrkt@gmail.com>
2024-06-02 19:52:29 +0300

    update

--- js/index.js
+++ js/index.js
@@ -27,7 +27,13 @@ function handleClick(e) {
   const itemsText = [...listStagesItems.children];
   items.forEach(el => el.classList.remove('is-active'));
   itemsText.forEach(el => el.classList.add('item-hidden'));
+  itemsText.forEach(el => el.children[0].classList.remove('is-active'));
+  itemsText.forEach(el => el.children[1].classList.remove('is-active'));
+
   itemsText[e.target.id - 1].classList.remove('item-hidden');
+  itemsText[e.target.id - 1].children[0].classList.add('is-active');
+  itemsText[e.target.id - 1].children[1].classList.add('is-active');
+
   e.target.classList.add('is-active');
 }
 
@@ -41,10 +47,7 @@ svgContainer.forEach(el => {
 });
 
 function addTextAnswer(e) {
-  console.log(answerText);
   const id = e.target.id || e.target.parentElement.id;
-  answerText[id - 1].classList.toggle('hidden');
-
   if (e.target.nodeName === 'use') {
     svgMinus[id - 1].classList.toggle('is-hidden');
     plusSvg[id - 1].classList.toggle('is-hidden');
@@ -52,6 +55,12 @@ function addTextAnswer(e) {
   }
   svgMinus[id - 1].classList.toggle('is-hidden');
   plusSvg[id - 1].classList.toggle('is-hidden');
+  if (answerText[id - 1].style.maxHeight) {
+    answerText.forEach(el => (el.style.maxHeight = null));
+  } else {
+    answerText.forEach(el => (el.style.maxHeight = null));
+    answerText[id - 1].style.maxHeight = answerText[id - 1].scrollHeight + 'px';
+  }
 }
 
 const casesContainer = document.querySelector('.js-cases__list');
@@ -73,28 +82,121 @@ function handleButtonLoadMore() {
   buttonLoadMore.removeEventListener('click', handleButtonLoadMore);
 }
 
+// ============================  MOUSE   =========================================
+
 const links = document.querySelectorAll('.js-cursor-hover');
 const span = document.querySelectorAll('.js-hover-mouse');
 
 console.log(span);
 links.forEach(link => link.addEventListener('mousemove', handleMouseMove));
 
-let x;
-let y;
+let mouseX;
+let mouseY;
+let containerMouseX = 0;
+let containerMouseY = 0;
+let containerMouse;
+
+let prev;
+let animationFrameId;
+
+let isMoving = false;
 function handleMouseMove(e) {
+  if (prev !== e.target) {
+    isMoving = false;
+    cancelAnimationFrame(animationFrameId);
+  }
+  prev = e.target;
   const container = e.target.getBoundingClientRect();
-  console.log(container);
-  const x = e.clientX - container.left;
-  const y = e.clientY - container.top;
-
-  const spanWidth = e.target.children[3].offsetWidth;
-  const spanHeight = e.target.children[3].offsetHeight;
-
-  let newX = x - spanWidth / 2;
-  let newY = y - spanHeight / 2;
-
-  e.target.children[3].style.left = newX + 'px';
-  e.target.children[3].style.top = newY + 'px';
-
-  console.log(`Mouse: (${x}, ${y})`);
+  mouseX = e.clientX - container.left;
+  mouseY = e.clientY - container.top;
+  containerMouse = e.target.children[3];
+  if (!isMoving) {
+    containerMouseX = mouseX - containerMouse.offsetWidth / 2;
+    containerMouseY = mouseY - containerMouse.offsetHeight / 2;
+    containerMouse.style.left = containerMouseX + 'px';
+    containerMouse.style.top = containerMouseY + 'px';
+    moveMouse();
+  }
 }
+
+function moveMouse() {
+  isMoving = true;
+
+  const distanceX = mouseX - containerMouseX;
+  const distanceY = mouseY - containerMouseY;
+  const spanWidth = containerMouse.offsetWidth;
+  const spanHeight = containerMouse.offsetHeight;
+  containerMouseX += (distanceX - spanWidth / 2) * 0.1;
+  containerMouseY += (distanceY - spanHeight / 2) * 0.1;
+
+  containerMouse.style.left = containerMouseX + 'px';
+  containerMouse.style.top = containerMouseY + 'px';
+
+  if (Math.abs(distanceX) > 1 || Math.abs(distanceY) > 1) {
+    animationFrameId = requestAnimationFrame(moveMouse);
+    console.log(1);
+  } else {
+    isMoving = false;
+  }
+}
+
+//  ЛІЧИЛЬНИК
+
+const numbers = document.querySelectorAll('.js-number');
+function counters() {
+  let oldValueMouth = numbers[0].innerText;
+  let valueMouth = 1;
+  numbers[0].innerHTML = valueMouth;
+  const counterMouth = setInterval(() => {
+    if (valueMouth >= oldValueMouth) {
+      clearInterval(counterMouth);
+      return;
+    }
+    valueMouth += 1;
+    numbers[0].innerHTML = valueMouth;
+  }, 300);
+
+  let oldValue = numbers[1].innerText;
+  let newValue = oldValue.slice(0, oldValue.length - 1);
+  let valueProgects = 0;
+  const counter = setInterval(() => {
+    if (valueProgects >= newValue) {
+      clearInterval(counter);
+      return;
+    }
+    valueProgects += 1;
+    numbers[1].innerHTML = `${valueProgects}<span>+</span>`;
+  }, 50);
+
+  let oldValueExperts = numbers[2].innerText;
+  let valueExperts = 0;
+  const counterExperts = setInterval(() => {
+    if (valueExperts >= oldValueExperts) {
+      clearInterval(counterExperts);
+      return;
+    }
+    valueExperts += 1;
+    numbers[2].innerHTML = valueExperts;
+  }, 100);
+}
+// OBSERVER
+
+const target = document.querySelector('.js-figures-section');
+
+const options = {
+  rootMargin: '300px',
+  threshold: 1.0,
+};
+
+const observer = new IntersectionObserver(hanglerload, options);
+observer.observe(target);
+function hanglerload(entries) {
+  entries.forEach(({ isIntersecting }) => {
+    if (isIntersecting) {
+      counters();
+      observer.unobserve(target);
+    }
+  });
+}
+
+// ===================
